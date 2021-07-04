5770cd908695adf94fdf4f63d0dfc6257668c792
ernsykamethelus <69830819+ernsykamethelus@users.noreply.github.com>
2021-07-04 19:57:08 -0400

    updates

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,12 +1,8 @@
 class BlogsController < ApplicationController
-    # before_action :require_user
+    before_action :require_user
     def new 
         @blog = Blog.new 
     end
-
-    # def index 
-    #     @blogs = Blogs.alpha
-    # end
 
     def about 
     end
